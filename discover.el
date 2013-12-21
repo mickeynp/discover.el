4b28a6517dd537c5d4b1e3018b1f7f35346a03e6
Steve Purcell <steve@sanityinc.com>
2013-12-21 20:19:20 +0000

    Add Package-Requires header for ELPA installations

--- discover.el
+++ discover.el
@@ -4,6 +4,7 @@
 
 ;; Author: Mickey Petersen <mickey@fyeah.org>
 ;; Keywords:
+;; Package-Requires: ((makey "0.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
