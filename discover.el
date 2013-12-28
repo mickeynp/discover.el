dcd1ae136a11feb932ccdfb9252c9b69ee19df8b
Johan Andersson <johan.rejeep@gmail.com>
2013-12-28 12:00:55 +0100

    Add package version header.

--- discover.el
+++ discover.el
@@ -4,6 +4,7 @@
 
 ;; Author: Mickey Petersen <mickey@fyeah.org>
 ;; Keywords:
+;; Version: 0.1
 ;; Package-Requires: ((makey "0.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
